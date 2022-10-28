38a6f7a3326683d5fe87de87bd3fc70eb9c641db
unknown <afsahmr@gmail.com>
2022-10-27 20:57:54 -0400

    'take8'

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 
 function Login(props) {
 
+    const navigate = useNavigate();
     const [loginForm, setloginForm] = useState({
       email: "",
       password: ""
